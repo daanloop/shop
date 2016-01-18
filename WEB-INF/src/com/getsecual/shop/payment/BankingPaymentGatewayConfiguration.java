5c6ddd87af8e0974620c2ab259540a9239055c23
maedomari <pc>
2016-01-18 16:02:22 +0900

    action/form 呼び出し対応

--- WEB-INF/src/com/getsecual/shop/payment/BankingPaymentGatewayConfiguration.java
+++ WEB-INF/src/com/getsecual/shop/payment/BankingPaymentGatewayConfiguration.java
@@ -7,6 +7,7 @@ import com.gmo_pg.g_pay.client.common.PaymentException;
 import com.gmo_pg.g_pay.client.impl.PaymentClientImpl;
 import com.gmo_pg.g_pay.client.input.EntryExecTranInput;
 import com.gmo_pg.g_pay.client.output.EntryExecTranOutput;
+import com.gmo_pg.g_pay.client.output.SecureTranOutput;
 
 /**
  * ◇カード番号を入力して決済する＜本人認証サービスを未使用＞
@@ -21,13 +22,35 @@ public class BankingPaymentGatewayConfiguration extends EntryExecTranInput imple
 
 	// エラー情報
 	private List entryErrList = null;
+	// 3Dセキュア認証結果
+	private String paRes = null;
+	// 取引ID
+	private String md = null;
+	// ACS呼出判定
+	private String acs;
+	// 本人認証パスワード入力画面URL
+	private String acsUrl;
+	// 本人認証サービス要求電文
+	private String paReq;
 
 	/**
 	 * 決済実行
 	 * @throws com.gmo_pg.g_pay.client.common.PaymentException
 	 */
 	public void executePaymentGateway() throws PaymentException {
+		// GMOPG
 		PaymentClientImpl paymentClientImpl = new PaymentClientImpl();
+
+		// 認証後決済(3D セキュア認証結果と取引IDが設定されている場合)
+		if (paRes != null && md != null) {
+			SecureTranOutput secureTranOutput = paymentClientImpl.doSecureTran(paRes, md);
+			// 結果確認
+			if (secureTranOutput.isErrorOccurred()) {
+				entryErrList = new ArrayList();
+				entryErrList.addAll(secureTranOutput.getErrList());
+			}
+			return;
+		}
 
 		// 決済実行
 		EntryExecTranOutput entryExecTranOutput = paymentClientImpl.doEntryExecTran(this);
@@ -37,17 +60,68 @@ public class BankingPaymentGatewayConfiguration extends EntryExecTranInput imple
 			entryErrList = new ArrayList();
 			entryErrList.addAll(entryExecTranOutput.getEntryErrList());
 			entryErrList.addAll(entryExecTranOutput.getExecErrList());
-
 		}
+
+		// 3D セキュア認証に必要な値を設定
+		this.acs = entryExecTranOutput.getAcs();
+		this.acsUrl = entryExecTranOutput.getAcsUrl();
+		this.paReq = entryExecTranOutput.getPaReq();
+		this.md = entryExecTranOutput.getMd();
+
 	}
 
-	// メソッド：executePaymentGatewayの戻り値なしですが、残高不足などのエラーはPaymentExceptionでスローされない為
-	// エラーコードとエラー情報の値は一時的に保持するようにしています。
 	/**
-	 * エラー情報を返却する
+	 * 3D セキュア認証結果を設定
+	 */
+	public void setPaRes(String paRes) {
+		this.paRes = paRes;
+	}
+	/**
+	 * 取引IDを設定
+	 */
+	public void setMd(String md) {
+		this.md = md;
+	}
+
+	/**
+	 * ACS呼出判定を取得
+	 * @return ACS呼出判定
+	 */
+	public String getAcs() {
+		return null;
+	}
+
+	/**
+	 * 本人認証パスワード入力画面URLを取得
+	 * @return 本人認証パスワード入力画面URL
+	 */
+	public String getAcsUrl() {
+		return null;
+	}
+
+	/**
+	 * 本人認証サービス要求電文を取得
+	 * @return 本人認証サービス要求電文
+	 */
+	public String getPaReq() {
+		return null;
+	}
+
+	/**
+	 * 取引IDを取得
+	 * @return 取引ID
+	 */
+	public String getMd() {
+		return null;
+	}
+
+	/**
+	 * エラー情報を取得
+	 * @return エラー情報
 	 */
 	public List getErrList() {
 		return this.entryErrList;
 	}
+
 
 }
--- WEB-INF/src/com/getsecual/shop/payment/PaymentGatewayConfiguration.java
+++ WEB-INF/src/com/getsecual/shop/payment/PaymentGatewayConfiguration.java
@@ -17,6 +17,50 @@ public interface PaymentGatewayConfiguration {
 	 */
 	public void executePaymentGateway() throws PaymentException;
 
+	// 取引情報
+	public void setShopId(String shopId);					// ショップID
+	public void setShopPass(String shopPass);				// ショップパスワード
+	public void setOrderId(String orderId);					// オーダーID ショップ発行の取引識別 ID
+	public void setJobCd(String jobCd);						// 処理区分
+	public void setItemCode(String itemCode);				// 商品コード 省略時はシステム固定値("0000990")
+	public void setAmount(Integer amount);					// 利用金額 『有効性チェック』を除く処理区分は必須
+	public void setTax(Integer tax);						// 税送料
+	public void setTdFlag(String tdFlag);					// セキュア使用フラグ 0:行なわない(デフォルト)
+	public void setTdTenantName(String tdTenantName);		// 3D セキュア表示店舗名
+	// 決済情報
+	public void setAccessId(String accessId);				// 取引ID
+	public void setAccessPass(String accessPass);			// 取引パスワード
+	public void setMethod(String method);					// 支払方法 ※『有効性チェック』を除く処理区分は必須。
+	public void setPayTimes(Integer payTimes);				// 支払回数 ※分割もしくはボーナス分割の支払方法は必須。
+	public void setCardNo(String cardNo);					// カード番号
+	public void setExpire(String expire);					// 有効期限
+	public void setSecurityCode(String securityCode);		// セキュリティーコード
+	public void setClientField1(String clientField1);		// 加盟店自由項目 1
+	public void setClientField2(String clientField2);		// 加盟店自由項目 2
+	public void setClientField3(String clientField3);		// 加盟店自由項目 3
+	public void setClientFieldFlag(String clientFieldFlag);	// 加盟店自由項目返却フラグ
+	public void setHttpAccept(String httpAccept);			// HTTP_ACCEPT
+	public void setHttpUserAgent(String httpUserAgent);		// HTTP_USER_AGENT
+	public void setDeviceCategory(String deviceCategory);	// 使用端末情報
+	public void setSiteId(String siteId);					// サイトID
+	public void setSitePass(String sitePass);				// サイトパスワード
+	public void setMemberId(String memberId);				// 会員ID
+	public void setSeqMode(String seqMode);					// カード登録連番モード
+	public void setCardSeq(Integer cardSeq);				// カード登録連番
+	public void setCardPass(String cardPass);				// カードパスワード
+	// 3Dセキュア認証後の決済で使用
+	public void setPaRes(String paRed);						// 3D セキュア認証結果
+	public void setMd(String md);							// 取引ID
+
+	// 決済の処理で失敗して、同じオーダーIDで再度決済を行う場合は必要
+	public String getAccessId();							// 取引ID
+	public String getAccessPass();							// 取引パスワード
+	// 出力値（3D認証に必要な値)
+	public String getAcs();									// ACS呼出判定(1:呼出要)
+	public String getAcsUrl();								// 本人認証パスワード入力画面URL
+	public String getPaReq();								// 本人認証サービス要求電文
+	public String getMd();									// 取引ID
+
 	/**
 	 * エラー情報取得
 	 * @return エラー情報のリスト
